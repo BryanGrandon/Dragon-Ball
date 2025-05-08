df91588ea319b5440a5c081fa3b254252a75e242
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-05-08 18:00:35 -0400

    feat(web):add header

--- src/services/dragonBall.ts
+++ src/services/dragonBall.ts
@@ -1,7 +1,7 @@
 import type { APIDragonBallResponse } from '../types/api'
 
 const getCharacters = async () => {
-  const response = await fetch('https://dragonball-api.com/api/characters?limit=12')
+  const response = await fetch('https://dragonball-api.com/api/characters?limit=15')
   const { items } = (await response.json()) as APIDragonBallResponse
   if (!response.ok) {
     throw new Error('Failed to fetch characters')
