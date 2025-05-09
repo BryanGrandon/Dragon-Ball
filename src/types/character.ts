0754c11fe0a9a9ab9950e1e8668c231b6d58abf2
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-05-09 13:44:35 -0400

    feat(web):add [id].astro for character page

---
+++ src/types/character.ts
@@ -0,0 +1,31 @@
+export interface APICharacterResponse {
+  readonly id: number
+  readonly name: string
+  readonly ki: string
+  readonly maxKi: string
+  readonly race: string
+  readonly gender: string
+  readonly description: string
+  readonly image: string
+  readonly affiliation: string
+  readonly deletedAt: null
+  readonly originPlanet: OriginPlanet
+  readonly transformations: Transformation[]
+}
+
+export interface OriginPlanet {
+  readonly id: number
+  readonly name: string
+  readonly isDestroyed: boolean
+  readonly description: string
+  readonly image: string
+  readonly deletedAt: null
+}
+
+export interface Transformation {
+  readonly id: number
+  readonly name: string
+  readonly image: string
+  readonly ki: string
+  readonly deletedAt: null
+}
