13dbb7a18325331565ad4f38fa103648b5d7b7a5
Bryan Grandon <bryan.grandon.c@gmail.com>
2025-05-16 16:23:08 -0400

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://BryanGrandon.github.io',
+  base: '/Dragon-Ball',
+
   vite: {
     plugins: [tailwindcss()],
   },
-
-  output: 'server',
-  integrations: [react()],
 })
